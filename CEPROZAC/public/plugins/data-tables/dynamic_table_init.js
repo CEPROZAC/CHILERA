912efb1f4c9a6fddf55c14cd4aac9192d16db21b
luisviramontes <luis_alfonso133@hotmail.com.com>
2018-03-11 20:10:58 -0600

    DynamicTableClientes

--- CEPROZAC/public/plugins/data-tables/dynamic_table_init.js
+++ CEPROZAC/public/plugins/data-tables/dynamic_table_init.js
@@ -145,3 +145,76 @@ $(document).ready(function() {
         }
     } );
  } );
+
+function fnFormatDetails2 ( oTable, nTr )
+{
+    var aData = oTable.fnGetData( nTr );
+    var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+
+    sOut += '<tr><td><strong>Nombre:</strong></td><td>'+aData[1]+' </td></tr>';
+    sOut += '<tr><td><strong>RFC:</strong></td><td>'+aData[2]+' </td></tr>';
+    sOut += '<tr><td><strong>Regimen Fiscal:</strong></td><td>'+aData[3]+' </td></tr>';
+    sOut += '<tr><td><strong>Telefono:</strong></td><td>'+aData[4]+' </td></tr>';
+    sOut += '<tr><td><strong>Correo:</strong></td><td>'+aData[5]+' </td></tr>';
+    sOut += '<tr><td><strong>Direccion de Facturación:</strong></td><td>'+aData[6]+' </td></tr>';
+    sOut += '<tr><td><strong>Direccion de Entrega de Embarques:</strong></td><td>'+aData[7]+' </td></tr>';
+    sOut += '<tr><td><strong>Asignación de Volumen de Venta por Año:</strong></td><td>'+aData[8]+' </td></tr>';
+    sOut += '<tr><td><strong>Saldo Cliente:</strong></td><td>'+aData[9]+' </td></tr>';
+
+    sOut += '</table>';
+
+    return sOut;
+}
+
+$(document).ready(function() {
+
+    $('#dynamic-table2').dataTable( {
+        "aaSorting": [[ 4, "desc" ]]
+    } );
+
+    /*
+     * Insert a 'details' column to the table
+     */
+     var nCloneTh = document.createElement( 'th' );
+     var nCloneTd = document.createElement( 'td' );
+     nCloneTd.innerHTML = '<img src="plugins/advanced-datatable/images/details_open.png">';
+     nCloneTd.className = "center";
+
+     $('#hidden-table-info2 thead tr').each( function () {
+        this.insertBefore( nCloneTh, this.childNodes[0] );
+    } );
+
+     $('#hidden-table-info2 tbody tr').each( function () {
+        this.insertBefore(  nCloneTd.cloneNode( true ), this.childNodes[0] );
+    } );
+
+    /*
+     * Initialse DataTables, with no sorting on the 'details' column
+     */
+     var oTable = $('#hidden-table-info2').dataTable( {
+        "aoColumnDefs": [
+        { "bSortable": false, "aTargets": [ 0 ] }
+        ],
+        "aaSorting": [[1, 'asc']]
+    });
+
+    /* Add event listener for opening and closing details
+     * Note that the indicator for showing which row is open is not controlled by DataTables,
+     * rather it is done here
+     */
+     $('#hidden-table-info2 tbody td img').click(function () {
+        var nTr = $(this).parents('tr')[0];
+        if ( oTable.fnIsOpen(nTr) )
+        {
+            /* This row is already open - close it */
+            this.src = "plugins/advanced-datatable/images/details_open.png";
+            oTable.fnClose( nTr );
+        }
+        else
+        {
+            /* Open this row */
+            this.src = "plugins/advanced-datatable/images/details_close.png";
+            oTable.fnOpen( nTr, fnFormatDetails2(oTable, nTr), 'details' );
+        }
+    } );
+ } );
