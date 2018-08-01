10b01592fc32a0c055a6ea6a816484b580019231
jesusrv1103 <jesus21c@hotmail.com>
2018-08-01 09:13:27 -0500

    necesario

---
+++ resources/views/almacen/agroquimicos/create.blade.php
@@ -0,0 +1,202 @@
+@extends('layouts.principal')
+@section('contenido')
+<div class="pull-left breadcrumb_admin clear_both">
+  <div class="pull-left page_title theme_color">
+    <h1>Inicio</h1>
+    <h2 class="">Agroquímicos</h2>
+  </div>
+  <div class="pull-right">
+    <ol class="breadcrumb">
+      <li><a style="color: #808080" href="{{url('almacenes/agroquimicos')}}">Inicio</a></li>
+      <li><a style="color: #808080" href="{{url('almacenes/agroquimicos')}}">Almacén de Agroquímicos</a></li>
+    </ol>
+  </div>
+</div>
+<div class="container clear_both padding_fix">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="block-web">
+        <div class="header">
+          <div class="row" style="margin-top: 15px; margin-bottom: 12px;">
+            <div class="col-sm-8">
+              <div class="actions"> </div>
+              <h2 class="content-header" style="margin-top: -5px;"><strong>Registrar Nuevo Producto</strong></h2>
+            </div>
+            <div class="col-md-4">
+              <div class="btn-group pull-right">
+                <div class="actions"> 
+                </div>
+              </div>
+            </div>    
+          </div>
+        </div>
+         <div class="porlets-content">
+         <div class="text-success" id='result'>
+          @if(Session::has('message'))
+          {{Session::get('message')}}
+          @endif
+        </div>
+          <form action="{{route('almacenes.agroquimicos.store')}}" method="post" class="form-horizontal row-border" parsley-validate novalidate  files="true" enctype="multipart/form-data" accept-charset="UTF-8">
+            {{csrf_field()}}
+
+
+            <div class="form-group">
+              <label class="col-sm-3 control-label">Nombre: <strog class="theme_color">*</strog></label>
+              <div class="col-sm-6">
+                <input name="nombre" type="text"  value="{{Input::old('nombre')}}" maxlength="30"  onchange="mayus(this);"  class="form-control" required value="" placeholder="Ingrese nombre del producto" />
+                 <div class="text-danger" id='error_nombre'>{{$errors->formulario->first('nombre')}}</div>
+
+              </div>
+            </div>
+
+              <div class="form-group">
+            <label class="col-sm-3 control-label"> Proveedor: <strog class="theme_color">*</strog></label>
+            <div class="col-sm-6">
+              <select name="provedor_name" class="form-control" value="{{Input::old('provedor_name')}}" required>  
+                @foreach($provedor as $provedores)
+                <option value="{{$provedores->id}}">
+                 {{$provedores->nombre}}
+               </option>
+               @endforeach              
+             </select>
+             <div class="help-block with-errors"></div>
+             <div class="text-danger" id='error_prov'>{{$errors->formulario->first('provedor_name')}}</div>
+           </div>
+         </div><!--/form-group-->
+
+
+
+            <div class="form-group">
+              <label class="col-sm-3 control-label">Codigo de Barras: <strog class="theme_color">*</strog></label>
+              <div class="col-sm-6">
+<input type="radio" value="1" name="habilitarDeshabilitar" onchange="habilitar(this.value);" checked> Ingrese Codigo de Barras 
+<input type="radio" value="2" name="habilitarDeshabilitar"  onchange="habilitar(this.value);"> GenerarCodigo de Barras Automatico
+
+<input type="radio" value="3" name="habilitarDeshabilitar"  onchange="habilitar(this.value);"> Ninguno
+
+              </div>
+            </div>
+
+ <div class="form-group">
+              <label class="col-sm-3 control-label"> <strog class="theme_color">*</strog></label>
+              <div class="col-sm-6">
+ <input type="text" name="codigo" id="segundo"  maxlength="35"   class="form-control" placeholder="Ingrese el Codigo de Barras" onkeypress=" return soloNumeros(event);" required value="{{Input::old('codigo')}}"/><br>
+ <div class="text-danger" id='error_rfc'>{{$errors->formulario->first('codigo')}}</div>
+</div>
+</div>
+
+    <div class="form-group ">
+      <label class="col-sm-3 control-label">Imagen</label>
+      <div class="col-sm-6">
+       <input  name="imagen" type="file"  value="{{Input::old('imagen')}}" accept=".jpg, .jpeg, .png" >
+     </div>
+   </div>
+   
+
+
+
+    <div class="form-group">
+              <label class="col-sm-3 control-label">Descripción: <strog class="theme_color">*</strog></label>
+              <div class="col-sm-6">
+                <input name="descripcion" type="text"  value="{{Input::old('descripcion')}}"  maxlength="70"  onchange="mayus(this);"  class="form-control" required value="" placeholder="Ingrese Descripción del Material" />
+              </div>
+            </div>
+
+               <div class="form-group">
+              <label  class="col-sm-3 control-label">Cantidad en Almacén <strog class="theme_color">*</strog></label>
+              <div class="col-sm-6">
+                <input name="cantidad" maxlength="9" type="number" value="{{Input::old('cantidad')}}" min="1" max='9999999' step="1" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control currency" required value="" placeholder="Ingrese la Cantidad en Almacén" onkeypress=" return soloNumeros(event);" />
+               </div>    
+               </div>  
+
+               <div class="form-group">
+            <label class="col-sm-3 control-label">Medida de Salida: <strog class="theme_color">*</strog></label>
+            <div class="col-sm-6">
+              <select name="medida" value="{{Input::old('medida')}}">
+                @if(Input::old('medida')=="Kilogramos")
+                <option value='Kilogramos' selected>Kilogramos
+                </option>
+                <option value="Toneladas">Toneladas</option>
+                <option value="Litros">Litros</option>
+                <option value="Metros">Metros</option>
+                <option value="Unidades">Unidades</option>
+                @elseif(Input::old('medida')=="Toneladas")
+                <option value='Toneladas' selected>Toneladas
+                </option>
+                <option value="Litros">Litros</option>
+                <option value="Metros">Metros</option>
+                <option value="Unidades">Unidades</option>
+                <option value='Kilogramos'>Kilogramos</option>
+                 @elseif(Input::old('medida')=="Litros")
+                <option value='Toneladas'>Toneladas</option>
+                <option value="Litros" selected>Litros</option>
+                <option value="Metros">Metros</option>
+                <option value="Unidades">Unidades</option>
+                <option value='Kilogramos'>Kilogramos</option>
+                @elseif(Input::old('medida')=="Metros")
+                <option value='Toneladas'>Toneladas</option>
+                <option value="Litros">Litros</option>
+                <option value="Metros" selected>Metros</option>
+                <option value="Unidades">Unidades</option>
+                <option value='Kilogramos'>Kilogramos</option>
+                @else
+                <option value='Toneladas'>Toneladas</option>
+                <option value="Litros">Litros</option>
+                <option value="Metros" >Metros</option>
+                <option value="Unidades" selected>Unidades</option>
+                <option value='Kilogramos'>Kilogramos</option>
+                @endif
+              </select>
+              
+            </div>
+          </div>
+
+                         <div class="form-group">
+              <label  class="col-sm-3 control-label">Stock Minimo <strog class="theme_color">*</strog></label>
+              <div class="col-sm-6">
+                <input name="stock_min" maxlength="9" type="number" value="{{Input::old('stock_min')}}" min="1" max='9999999' step="1" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control currency" required value="" placeholder="Ingrese la Cantidad de Stock Minimo en Almacén" onkeypress=" return soloNumeros(event);" />
+               </div>    
+               </div> 
+
+
+   
+
+   <div class="form-group">
+    <div class="col-sm-offset-7 col-sm-5">
+      <button type="submit" class="btn btn-primary">Guardar</button>
+      <a href="{{url('/almacenes/agroquimicos')}}" class="btn btn-default"> Cancelar</a>
+    </div>
+  </div><!--/form-group-->
+
+
+</form>
+</div><!--/porlets-content-->
+</div><!--/block-web-->
+</div><!--/col-md-12-->
+</div><!--/row-->
+</div>
+@endsection
+
+<script>
+function habilitar(value)
+{
+if(value=="1")
+{
+// habilitamos
+document.getElementById("segundo").disabled=false;
+  document.getElementById("segundo").value = "";
+   document.getElementById("segundo").focus(); 
+}else if(value=="2"){
+// deshabilitamos
+document.getElementById("segundo").disabled=false;
+document.getElementById("segundo").readonly="readonly";
+document.getElementById("segundo").readonly=true;
+var aleatorio = Math.floor(Math.random()*999999999999);
+document.getElementById("segundo").value=aleatorio;
+}else if (value=="3"){
+  document.getElementById("segundo").disabled=true;
+    document.getElementById("segundo").value = "";
+}
+}
+</script>
+</head>
---
+++ resources/views/almacen/agroquimicos/modale.blade.php
@@ -0,0 +1,194 @@
+<div class="modal fade modal-slide-in-right" aria-hidden="true" role="dialog" tabindex="-1" id="modal-delete2-{{$materiales->id}}">
+  <div class="modal-dialog">
+    <div class="modal-content panel default blue_border horizontal_border_1">
+      <div class="modal-body"> 
+        <div class="row">
+          <div class="block-web">
+            <div class="header">
+              <h3 class="content-header theme_color">&nbsp;Agregar Stock: {{ $materiales->nombre}}</h3>
+            </div>
+
+
+            <div class="porlets-content" style="margin-bottom: -50px;">
+          <form action="{{url('almacenes/agroquimicos/stock', [$materiales->id])}}" method="post" class="form-horizontal row-border" parsley-validate novalidate files="true" enctype="multipart/form-data" accept-charset="UTF-8">
+            {{csrf_field()}}
+            <input type="hidden" name="_method" value="PUT">
+
+
+
+              <div class="form-group">
+              <label  class="col-sm-3 control-label">Agregar Stock<strog class="theme_color">*</strog></label>
+              <div class="col-sm-6">
+                <input name="cantidades" id="cantidades" maxlength="9" type="text" value="1" min="1" max='9999999'" class="form-control" required placeholder="Ingrese la Cantidad" onkeypress=" return soloNumeros(event);" />
+               </div>    
+               </div>
+                           <br> <br>
+
+      <div class="form-group">
+        <label class="col-sm-3 control-label">Fecha de Compra<strog class="theme_color">*</strog></label>
+        <div class="col-sm-6">
+
+         <input type="date" name="fecha2" id="fecha2" value="" class="form-control mask" >
+       </div>
+     </div>
+
+            <br> <br>
+
+       <div class="form-group">
+          <label class="col-sm-3 control-label">Empresa : <strog class="theme_color">*</strog></label>
+          <div class="col-sm-6">
+            <select name="recibio" id="recibio" value="recibio"  class="form-control select" required>  
+              @foreach($empresas as $emp)
+              <option value="{{$emp->nombre}}">
+               {{$emp->nombre}} 
+             </option>
+             @endforeach              
+           </select>
+           <div class="help-block with-errors"></div>
+         </div>
+       </div>
+       <br> <br>
+
+                <div class="form-group">
+          <label class="col-sm-3 control-label">Entregado a : <strog class="theme_color">*</strog></label>
+          <div class="col-sm-6">
+            <select name="entregado_a" id="entregado_a" value=""  class="form-control select" required>  
+              @foreach($empleado as $emp)
+              <option value="{{$emp->id}}">
+               {{$emp->nombre}} {{$emp->apellidos}} 
+             </option>
+             @endforeach              
+           </select>
+           <div class="help-block with-errors"></div>
+         </div>
+       </div>
+       <br> <br>
+
+
+                       <div class="form-group">
+          <label class="col-sm-3 control-label">Recibe en Almacén CEPROZAC : <strog class="theme_color">*</strog></label>
+          <div class="col-sm-6">
+            <select name="recibe_alm" id="recibe_alm" value=""  class="form-control select" required>  
+              @foreach($empleado as $emp)
+              <option value="{{$emp->id}}">
+               {{$emp->nombre}} {{$emp->apellidos}} 
+             </option>
+             @endforeach              
+           </select>
+           <div class="help-block with-errors"></div>
+         </div>
+       </div>
+       <br> <br>
+       <br> <br>
+
+              <div class="form-group">
+  <label class="col-sm-3 control-label">Observaciónes: <strog class="theme_color">*</strog></label>
+  <div class="col-sm-6">
+
+    <input name="observaciones" type="text"  maxlength="200" onchange="mayus(this);"  class="form-control" onkeypress=" return soloLetras(event);" value="" placeholder="Ingrese Observaciónes de la Compra"/>
+  </div>
+</div>
+            <br> <br>
+       <div class="form-group">
+        <label class="col-sm-3 control-label">Número de Factura: <strog class="theme_color">*</strog></label>
+        <div class="col-sm-6">
+          <input name="factura" id="factura" value="" type="text"  maxlength="10" onchange="mayus(this);"  class="form-control" onkeypress=" return soloNumeros(event);"  value="" placeholder="Ingrese el Número de Factura"/>
+        </div>
+      </div>
+            <br> <br>
+      <div class="form-row">    
+          <label class="col-sm-3 control-label">Precio Unitario <strog class="theme_color">*</strog></label>
+          <div class="col-sm-6">
+            <div class="input-group">
+             <div class="input-group-addon">$</div>
+
+             
+             <input name="preciou" id="preciou" maxlength="9" type="number" value="{{Input::old('preciou')}}" min="0" max='9999999' step="100" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control currency" required value="" placeholder="Ingrese el Precio de este Producto" onkeypress=" return soloNumeros(event);"/>
+           </div>
+         </div>
+       </div>
+<br> <br>
+<br> <br>
+        
+   <div class="form-group"> 
+     <label class="col-sm-3 control-label">% IVA  <strog class="theme_color">*</strog></label>
+      <div class="col-sm-6">
+    <input name="iva" id="iva" value="16" type="text" class="form-control" onkeypress=" return soloNumeros(event);" placeholder="Ingrese el % IVA del Producto" />
+  </div>    
+  </div>
+<br> <br>
+  <div class="form-group"> 
+     <label class="col-sm-3 control-label">% IEPS </label>
+     <div class="col-sm-6">
+    <input name="ieps" id="ieps" value="1" type="text" class="form-control" onkeypress=" return soloNumeros(event);" placeholder="Ingrese el % IEPS del Producto" />
+  </div>    
+</div>  
+
+<br> <br>
+
+          <div class="form-group">
+            <label class="col-sm-3 control-label">Tipo de Moneda: <strog class="theme_color">*</strog></label>
+            <div class="col-sm-6">
+              <select name="moneda"  id ="moneda" class="form-control select" data-live-search="true"  value="{{Input::old('moneda')}}">
+                   
+                @if(Input::old('moneda')=="Peso MXM")
+                <option value='Peso MXN' selected>Peso MXN
+                </option>
+                <option value="Dolar USD">Dolar USD</option>
+                @else
+                <option value='Dolar USD' selected>Dolar USD
+                </option>
+                <option value="Peso MXN">Peso MXN</option>
+                @endif
+              </select>          
+            </div>
+            </div>
+
+
+       
+    </div>
+            <br> <br>
+ 
+            </div><!--/porlets-content--> 
+          </div><!--/block-web--> 
+<br> <br>
+          <div>
+                   <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+         <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+         <button type="submit" onclick="return save();" class="btn btn-primary">Agregar</button>
+         </div>
+
+        </div>
+      </section>
+    </div>
+    
+         </form>
+ </div><!--/modal-content--> 
+</div><!--/modal-dialog--> 
+</div><!--/modal-fade--> 
+
+
+<script>
+  function save()
+  {
+    var w = document.getElementById('fecha2').value;
+    var x = document.getElementById('preciou').value;
+    var y = document.getElementById('factura').value;
+    var z = document.getElementById('cantidades').value;
+     var a = document.getElementById('iva').value;
+      var b = document.getElementById('ieps').value;
+
+    if (w !== "" && x !== "" && y !== "" && z !== "" & a !== "" & b !== "" ){
+      if (z < 1){
+         swal("Alerta!", "La Cantidad de Entrada debe ser Mayor de 0!", "error");
+        return false;
+      }
+return true;
+    }else{
+      swal("Alerta!", "Faltan campos Por llenar Favor de Verificar!", "error");
+      return false;
+
+    }
+
+}
+</script>
